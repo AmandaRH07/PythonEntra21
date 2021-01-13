0c93c41f8c3b703db5750e6b55b07e76e6b99a37
AmandaRH07 <amandarafahass13@gmail.com>
2021-01-13 19:57:49 -0300

    JavaScript

--- Aulas/Exercicios/aula17/main.js
+++ Aulas/Exercicios/aula17/main.js
@@ -1,31 +1,30 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector('.displayed-img')
+const thumbBar = document.querySelector(".thumb-bar")
+const button = document.querySelector("button")
+const overlay = document.querySelector(".overlay")
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
-/* Looping through images */
-
-for(let i = 1; i <= 5; i++) {
-  const newImage = document.createElement('img');
-  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
-  thumbBar.appendChild(newImage);
-  newImage.onclick = function(e) {
-    displayedImage.src = e.target.src;
+for(i = 1; i <= 5; i++){
+  // criar um elemento no html
+  const newImage = document.createElement('img')
+  newImage.setAttribute('src', 'images/pic'+ i + '.jpg')
+  // adiciona a nova imagem no thumb-bar
+  thumbBar.append(newImage)
+  // troca a imagem principal
+  newImage.onclick = function(e){
+    displayedImage.src = e.target.src
   }
 }
 
-/* Wiring up the Darken/Lighten button */
-
-btn.onclick = function() {
-  const btnClass = btn.getAttribute('class');
-  if(btnClass === 'dark') {
-    btn.setAttribute('class','light');
-    btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-  } else {
-    btn.setAttribute('class','dark');
-    btn.textContent = 'Darken';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+button.onclick = function(e){
+  const btn = btn.getAttribute('class')
+  if (btn === 'dark'){
+    btn.setAttribute('class','light')
+    btn.textContent = "Lighten"
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)"
+  } 
+  else{
+    btn.setAttribute('class','dark')
+    btn.textContent = "Darken"
+    overlay.style.backgroundColor = "rgba(0,0,0,0)"
   }
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# PythonEntra21
-Repositório para as aulas do Entra21, turma de Python 2020/2021
